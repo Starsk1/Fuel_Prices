e2c8e44b8b2dedcaf5fd6d8558e9d3dc8e23a8ac
Starsk1 <estarczewski1@sheffield.ac.uk>
2023-05-03 10:11:07 +0100

    Data wrangling and graph update

--- Code/Script_1.R
+++ Code/Script_1.R
@@ -12,17 +12,61 @@ library(readxl)
 
 
 
-###Load data
+###---------------------------------------------------------------Loading data--------------------------------------------------------
+
+## loading the workbook from the data file, skipping the first 7 rows and selcting the sheet "all years"
+
 
 raw_data <- read_excel(here('Data','Weekly_Fuel_Prices.xlsx'),skip=7,sheet='All years')
 
 
-#### this did not work -> read_excel('Weekly_Fuel_Prices.xlsx',skiprows=7,sheet='All years')
 
-
+###--------------------------------------------------------------Data Wrangling----------------------------------------------------------
 head(raw_data)
 
-getwd()
+###next steps are to: 
+###1) remove useless columns keeping just price per litre for diesel and petrol respectively - done
+###2) give better names to the variables ULSP = petrol, ULSD = diesel - done
+###3) round diesel and petrol up to 2 decimal places - to do 
+
+clean_data <- raw_data %>% 
+  rename("Petrol"="ULSP:  Pump price (p/litre)",
+         "Diesel"="ULSD: Pump price (p/litre)") %>% 
+  select(Date,Petrol,Diesel)
+
+###gonna pivot the table to make plotting petrol and diesel together easier 
+
+df <- pivot_longer(clean_data, 
+             -Date,
+             names_to="Cat",
+             values_to="Value")
 
 
-here()
+head(clean_data)
+
+###--------------------------------------------------------Creating the Visualization----------------------------------------------
+### What do I want from the graph 
+### 1) different colour lines for diesel and petrol
+### 2) legends descibing colour code, X and Y axis, and a title 
+### 3) axis is probably going to need adjusting 
+
+
+##ggplot(df=clean_data, 
+##       mapping=aes(x=Date,y=Petrol))
+##p+geom_smooth()
+
+####creates a basic graph 
+ggplot(df, aes(x=Date, 
+               y=Value,
+               colour=Cat))+
+  geom_line()+
+  labs(title="Fuel Prices Over the Past 20 Years",
+       x="Date", y="Price Per Litre (p)",
+       color="Fuel Type")
+
+
+
+#ggplot(data=clean_data, aes(x=Date))+
+#  geom_line(aes(y=Diesel), color="darkred")+
+#  geom_line(aes(y=Petrol), color="steelblue")
+
